cf7423ad0a10c1945d9175beb5d6d705b10224c6
Nicola <nicolavalente88@gmail.com>
2020-06-09 16:03:05 +0200

    Piano Piano

--- js/main.js
+++ js/main.js
@@ -1,1 +1,23 @@
 //myJavaScript
+$(document).ready(function () {
+
+  $(".next").click(
+    function(){
+        
+    }
+  );
+
+  $(".prev").click(
+    function(){
+
+    }
+  );
+});
+//FUNZIONI
+//cliccando su freccia destra dello slider la class .active viene tolta a img currentElement
+//la class .active viene aggiunta ad element img successivo dello slider
+//quando si arriva a last-child img element dello slider la class .active viene data a first-child img element
+
+//cliccando su freccia destra dello slider la class .active viene tolta a i.fa-circle currentElement
+//la class .active viene aggiunta ad i.fa-circle element successivo dello slider
+//quando si arriva a last-child i.fa-circle element dello slider la class .active viene data a first-child i.fa-circle element
